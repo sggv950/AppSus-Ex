090dd1472a0eb91e71a56945ff6c1a053d496722
sggv950 <38014323+sggv950@users.noreply.github.com>
2018-11-07 13:26:03 +0200

    commit

--- js/pages/email-app.cmp.js
+++ js/pages/email-app.cmp.js
@@ -1,40 +1,25 @@
 'use strict';
 import emailService from '../services/email.service.js'
-<<<<<<< HEAD
-import emailList from '../cmps/email-list.cmp.js'
-import emailDetails from './email-details.cmp.js'
-=======
 import emailList from '../cmps/email-list.cmp.js';
 import emailFilter from '../cmps/email-filter.cmp.js';
->>>>>>> 9c5ce968302343abb25000a4421820e224eb1d25
 
 export default {
     name: 'emailapp',
     template: `
         <section>
-            <h1>email-app</h1>
             <div class='email-app'>
-<<<<<<< HEAD
-                <email-list :mails="emails"></email-list>
-                <email-details class="email-details"></email-details>
-=======
                 <email-filter @filtered="setFilter"></email-filter>
                 <email-list :mails="emails" @filtered="setFilter"></email-list>
->>>>>>> 9c5ce968302343abb25000a4421820e224eb1d25
             </div>
         </section>
     `,
     data() {
         return {
             emails: null,
-<<<<<<< HEAD
-            selected
-=======
->>>>>>> 9c5ce968302343abb25000a4421820e224eb1d25
         }
     },
     created() {
-        showParams();
+        
         emailService.query()
             .then(emails => {
                 console.log(emails.inbox)
@@ -47,16 +32,10 @@ export default {
         }
     },
     methods: {
-        showParams(){
-            console.log(this.$route.params);
-        }
+        
     },
     components: {
         emailList,
-<<<<<<< HEAD
-        emailDetails
-=======
         emailFilter
->>>>>>> 9c5ce968302343abb25000a4421820e224eb1d25
     }
 }
