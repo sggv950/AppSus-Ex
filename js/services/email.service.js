9a7d790dba6a90d514f22083cd691ba5f663838d
sggv950 <38014323+sggv950@users.noreply.github.com>
2018-11-07 17:20:36 +0200

    chaned db structure and filter

--- js/services/email.service.js
+++ js/services/email.service.js
@@ -3,23 +3,21 @@ import utilService from './util.service.js'
 
 const KEY = 'emailsKey';
 
-function query(filter={keyword: '',type: 'Inbox',}) {
+function query(filter = { keyword: '', type: 'Inbox', }) {
     return storageService.load(KEY)
         .then(emails => {
             if (!emails || !emails.length) {
-                 emails = getEmails();
+                emails = getEmails();
                 storageService.store(KEY, emails);
             }
             var currEmails = emails
-            if(filter.type === 'Inbox') currEmails = currEmails.inbox;
-            else if(filter.type === 'Outbox') currEmails = currEmails.sent;
-            else if(filter.type == 'Drafts') currEmails = currEmails.drafts;
-             return currEmails.filter(email => email.from.toUpperCase().includes(filter.keyword.toUpperCase()))
-            //  .filter(email => email.subject.toUpperCase().includes(filter.keyword.toUpperCase()))
-        })    
+            return currEmails.filter(email => email.type.toUpperCase().includes(filter.type.toUpperCase()))
+                .filter(email => email.from.toUpperCase().includes(filter.keyword.toUpperCase()))
+                // .filter(email => email.subject.toUpperCase().includes(filter.keyword.toUpperCase()))
+        })
 }
 
-function getEmailById(id){
+function getEmailById(id) {
     return storageService.load(KEY)
         .then(emails => {
             console.log('service', id)
@@ -29,71 +27,83 @@ function getEmailById(id){
         })
 }
 
-function getEmails(){
-    return { inbox : [{
-        id: utilService.makeId(),
-        from: "david bruho",
-        subject:'fdfds',
-        body: "mi est eros convallis auctor arcu dapibus himenaeos",
-        time:Date.now(),
-        isRead: true,   
+
+
+function updateEmailRead(id) {
+    getEmailById(id)
+        .then(email => email.isRead = true)
+        .then()
+}
+
+function getEmails() {
+    return [{
+            id: utilService.makeId(),
+            from: "david bruho",
+            subject: 'fdfds',
+            body: "mi est eros convallis auctor arcu dapibus himenaeos",
+            time: Date.now(),
+            isRead: true,
+            type: 'Inbox'
         }
-      ,
-      {
-        id: utilService.makeId(),
-        from: "amir meyer",
-        subject: 'ice Cream',
-        body: "daslkdslajlskajdjaslkdskladlkasjdkasjkdjasldjaskjdlsajdlasjlksjaldjaskljlsajdkla",
-        time: Date.now(),
-        isRead: true,   
+            ,
+        {
+            id: utilService.makeId(),
+            from: "amir meyer",
+            subject: 'ice Cream',
+            body: "daslkdslajlskajdjaslkdskladlkasjdkasjkdjasldjaskjdlsajdlasjlksjaldjaskljlsajdkla",
+            time: Date.now(),
+            isRead: true,
+            type: 'Inbox'
         },
         {
-          id: utilService.makeId(),
-          from: "metus hendrerit",
-          subject:'asdasd asdsad',
-          body: "mi est eros convallis auctor arcu dapibus himenaeos",
-          time:Date.now(),
-          isRead: false,   
-          },
-          {
+            id: utilService.makeId(),
+            from: "metus hendrerit",
+            subject: 'asdasd asdsad',
+            body: "mi est eros convallis auctor arcu dapibus himenaeos",
+            time: Date.now(),
+            isRead: false,
+            type: 'Inbox'
+        },
+        {
             id: utilService.makeId(),
             from: "fafa asas",
-            subject:'fasfas',
+            subject: 'fasfas',
             body: "mi est eros convallis auctor arcu dapibus himenaeos",
-            time:Date.now(),
-            isRead: true,   
-            },
-            {
-              id: utilService.makeId(),
-              from: "asd dsa",
-              subject:'asdasdsa',
-              body: "mi est eros convallis auctor arcu dapibus himenaeos",
-              time:Date.now(),
-              isRead: false,   
-              },
-              {
-                id: utilService.makeId(),
-                from: "metus hendrerit",
-                subject:'asdasdad',
-                body: "mi est eros convallis auctor arcu dapibus himenaeos",
-                time:Date.now(),
-                isRead: false,   
-                },
-                {
-                  id: utilService.makeId(),
-                  from: "bruho chichi",
-                  subject:'asdasdsad',
-                  body: "dasdasdasdsadsadasdasge fas fa sf skaj  alskjd lksaj saldk jsadl jsadkl sja",
-                  time: Date.now(),
-                  isRead: false,   
-                  }
-                ],
-                sent:[],
-                drafts:[],
-                }
-            }
+            time: Date.now(),
+            isRead: true,
+            type: 'Inbox'
+        },
+        {
+            id: utilService.makeId(),
+            from: "asd dsa",
+            subject: 'asdasdsa',
+            body: "mi est eros convallis auctor arcu dapibus himenaeos",
+            time: Date.now(),
+            isRead: false,
+            type: 'Inbox'
+        },
+        {
+            id: utilService.makeId(),
+            from: "metus hendrerit",
+            subject: 'asdasdad',
+            body: "mi est eros convallis auctor arcu dapibus himenaeos",
+            time: Date.now(),
+            isRead: false,
+            type: 'Inbox'
+        },
+        {
+            id: utilService.makeId(),
+            from: "bruho chichi",
+            subject: 'asdasdsad',
+            body: "dasdasdasdsadsadasdasge fas fa sf skaj  alskjd lksaj saldk jsadl jsadkl sja",
+            time: Date.now(),
+            isRead: false,
+            type: 'Inbox'
+        }
+        ]
+}
 
-            export default {
-                query,
-                getEmailById
-            }
+export default {
+    query,
+    getEmailById
+}
