c863111aef948bc8fe47c03537e068958ce25985
thelastpako <44051881+thelastpako@users.noreply.github.com>
2018-11-07 20:47:33 +0200

    home

--- js/cmps/keep-preview.cmp.js
+++ js/cmps/keep-preview.cmp.js
@@ -3,11 +3,7 @@ export default {
     name: 'notepreview',
     props: ['note'],
     template: `
-    <div class="note-preview-item">
-        <div class="note-time">{{note.id}}</div>
-        <div class="note-name">{{note.type}}</div>
-        <div class="note-subject">{{note.text}}</div>
-    </div>
+    <component></component>
     `,
     data() {
         return{
