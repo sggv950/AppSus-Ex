65b42a85dd79c423d27d135bd4a5ed743e674ce8
thelastpako <pakokap1990@gmail.com>
2018-11-08 17:54:15 +0200

    dudi

--- js/cmps/email-list.cmp.js
+++ js/cmps/email-list.cmp.js
@@ -29,7 +29,6 @@ export default {
     methods:{
     showCount(count){
         if(count <= 0) return 0;
-<<<<<<< HEAD
         this.counter = count
     },
     sortByName(){
@@ -63,9 +62,6 @@ export default {
             return a.time - b.time;
           });
           storageService.store('emailsKey',this.mails)
-=======
-        this.counter = count;
->>>>>>> f069c2fada40fe9669fe705ab8b5eaa9953411cb
     }
     },
     components: {
